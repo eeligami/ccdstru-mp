174c7c42218ff2b5b782d20df0f211bc6c15149e
Eli Baldado <mackgb211@gmail.com>
2026-03-17 02:00:08 +0800

    Add function prototypes to ccdstru_mp.h
    Added function prototypes as comments in ccdstru_mp.h

--- ccdstru_mp.h
+++ ccdstru_mp.h
@@ -1,2 +1,11 @@
 #define MAX_TURNS 20
 #define GRID_SIZE 3
+
+/* Function prototypes (uncomment if function is fully implemented)
+ * void Remove(); 
+ * void Replace(); 
+ * void Expand(); 
+ * void Update(); 
+ * void NextPlayerMove(); 
+ * int GameOver(); // boolean function
+ */
