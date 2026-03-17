5ed20fc2d12fe8d982fc85daa6e07c7319653223
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-03-17 14:43:23 +0800

    Add GameState struct and include headers

--- ccdstru_mp.h
+++ ccdstru_mp.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_TURNS 20
 #define GRID_SIZE 3
 
@@ -9,3 +12,15 @@
  * void NextPlayerMove(); 
  * int GameOver(); // boolean function
  */
+
+typedef struct{
+  char board[3][3];
+  int K[3][3]; //1 or 0, tracks the squares that was touched during the current turn 
+  int D[3][3]; //1 or 0, tracks the squares that have already "expanded" or exploded
+
+  int good;
+  int go;
+  int found;
+  int over;
+  int val;
+}GameState;
