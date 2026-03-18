cc48253f5b0ced0a095430681e783a48ccbf661d
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-03-18 22:47:39 +0800

    Update ccdstru_mp.c

--- ccdstru_mp.c
+++ ccdstru_mp.c
@@ -3,6 +3,51 @@
 
 #include "ccdstru_mp.h"
 
+//System Intialization
+void InitGame(GameState *state)
+{
+  state->good = 0;
+  state->go = 1;
+  state->start = 1;
+  state->found = 0;
+  state->over = 0;
+  state->val = 0;
+
+  int i;
+  int j;
+  for (i = 0; i < GRID_SIZE; i++){
+    for (j = 0; j < GRID_SIZE; j++){
+      state->R[i][j] = 0; //R = empty
+      state->B[i][j] = 0; //B = empty
+      state->S[i][j] = 0; //S = empty
+      state->T[i][j] = 0; //T = empty
+    }
+  }
+}
+
+void RemovePos(GameState *state, int r, int c)
+{
+    if (r < 0 || r >= GRID_SIZE || c < 0 || c >= GRID_SIZE)
+    {
+    return;
+    }
+  
+    if (state->go)
+    {
+      state->R[r][c] = 0;
+    }
+    else
+    {
+      state->B[r][c] = 0;
+    }
+
+    state->S[r][c] = 0;
+    state->T[r][c] = 0;
+    
+  }
+  
+}
+
 int main()
 {
   int board[GRID_SIZE][GRID_SIZE];
