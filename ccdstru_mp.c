14440c6c3491285bf47abed6e1939243b2c15550
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-03-23 06:38:10 +0800

    Update ccdstru_mp.c

--- ccdstru_mp.c
+++ ccdstru_mp.c
@@ -32,6 +32,39 @@ void InitGame(GameState *state)
   }
 }
 
+void CheckGameOver(GameState *state)
+{
+  int countR = 0;
+  int countB = 0;
+  int countF = 0;
+  int i;
+  int j;
+
+  for (i = 0; i < GRID_SIZE; i++)
+    {
+      for (i = 0; i < GRID_SIZE; i++)
+      {
+        if (state->R[i][j])
+        {
+          countR++;  
+        }
+        if (state->B[i][j])
+        {
+          countB++;  
+        }
+        if (!state->R[i][j] && !state->B[i][j])
+        {
+          countF++;
+        }
+      }
+    }
+
+  if (countF = 0 || state->val >= 20 || (!state->start && ((countR > 0 && countB == 0) || (countR == 0 && countB > 0))))
+  {
+    start->over = 1;
+  }  
+}
+
 void RemovePos(GameState *state, int r, int c)
 {
     if (r < 0 || r >= GRID_SIZE || c < 0 || c >= GRID_SIZE)
@@ -55,9 +88,122 @@ void RemovePos(GameState *state, int r, int c)
   
 }
 
+void Expand(GameState *state, int a, int b)
+{
+
+}
+
+void Update(GameState *state, int r, int c)
+{
+  state->good = 0;
+
+  if (!state->S[r][c])
+  {
+    state->S[r][c] = 1;
+    state->good = !state->good;
+  }
+  else if (!state->good && state->S[r][c] && !state->T[r][c])
+  {
+    state->T[r][c] = 1;
+    Expand(state, r, c);
+  }
+}
+
+void NextPlayerMove(GameState *state, int r, int c)
+{
+  if (r < o || r >= GRID_SIZE || c < 0 || c >= GRID_SIZE)
+  {
+    return;
+  }
+
+  if (!state->over && state->start && state->go)
+  {
+    state->R[r][c] = 1;
+    state->S[r][c] = 1;
+    state->good = 1;
+  }
+  else if (!state->over && state->start && !state->go) 
+  {
+    state->B[r][c] = 1;
+    state->S[r][c] = 1;
+    state->good = 1;
+  }
+  else if(!state->over && !state->start && ((state->go && state->R[r][c]) || (!state->go && state->B[r][c])))
+  {
+    Update(state, r, c);
+    state->good = 1;
+  }
+
+  int countR = 0;
+  int countB = 0;
+  int i;
+  int j;
+  
+  for (i = 0; i < 3; i++)
+  {
+    for (j = 0; j < 3; j++)
+    {
+      if (state->R[i][j])
+      {
+        countR++;
+      }
+      if (state->B[i][j])
+      {
+        countB++;
+      }
+    }
+  }
+
+  if (state->start && countR == 1 && countB == 1)
+  {
+    state->start = 0;
+  }
+
+  if (!state->over && state->good)
+  {
+    state->good = !state->good;
+    state->go = !state->go;
+    state->val++;
+  }
+
+  CheckGameOver(state);
+}
+
+
+
 int main()
 {
-  int board[GRID_SIZE][GRID_SIZE];
+  GameState game;
+  InitGame(&game);
+
+  printf("Game Started!\n");
+  printf("Max Turns: %d\n\n", MAX_TURNS);
+
+  while (!game.over)
+    {
+      int r, c;
+      int currentTurn = game.val + 1;
+
+      printf("^-^-^ Turn %d / %d ^-^-^\n", currentTurn, MAX_TURNS);
+
+      if (game.go)
+      {
+        printf("Player R's move.\n");
+      }
+      else
+      {
+        printf("Player B's move.\n");
+      }
+
+      printf("Enter row and column (example: 1 1)");
+
+      scanf("%d %d", &r, &c);
+
+      NextPlayerMove(&game, r, c);
+
+      printf("\n");
+
+      
   
   return 0;
 }
