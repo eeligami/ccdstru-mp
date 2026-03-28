6ec2675044be690eb857ee1a8036d5999cdaf35d
eeligami <mackgb211@gmail.com>
2026-03-29 01:50:49 +0800

    Removed base function prototype comments

--- ccdstru_mp.h
+++ ccdstru_mp.h
@@ -1,15 +1,6 @@
 #define MAX_TURNS 20
 #define GRID_SIZE 3
 
-/* Function prototypes (uncomment if function is fully implemented)
- * void Remove(); 
- * void Replace(); 
- * void Expand(); 
- * void Update(); 
- * void NextPlayerMove(); 
- * int GameOver(); // boolean function
- */
-
 typedef struct{
   int R[GRID_SIZE][GRID_SIZE]; // Tracks all the grid coordinates currently owned by Player R
   int B[GRID_SIZE][GRID_SIZE]; // Tracks all the grid coordinates currently owned by Player B
